57c84ba802a25d0e045255a853c6f6a1e34f7468
Ann <58136902+RunWithMeNow@users.noreply.github.com>
2020-02-02 19:08:46 +0800

    Add files via upload

---
+++ ghjtrn/shmara.js
@@ -0,0 +1,7 @@
+let randomBodyParts=["Пидорасина по жизни","Мразь конченная","Сука ебаная"];
+let randomAffecties=["тупая","пизданутая", "жалкая"];
+let randomWords=["уебанка","крыса","прошмандовка"]
+let randomBodyPart=randomBodyParts[Math.floor(Math.random()*randomBodyParts.length)];
+let randomAffectiv=randomAffecties[Math.floor(Math.random()*randomAffecties.length)];
+let randomWord=randomWords[Math.floor(Math.random()*randomWords.length)];
+alert("Ты, "+randomBodyPart+" ,словно "+randomAffectiv+" "+randomWord+" , просто соси хуй)");
